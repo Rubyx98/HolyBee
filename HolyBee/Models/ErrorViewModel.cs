c8e298e0f86c6b75d780f95ad9f45e5dc1be2728
Joy <joyhaha@hotmail.nl>
2018-01-10 09:09:39 +0100

    Add project files.

---
+++ HolyBee/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HolyBee.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
